afc3b7b4afd91689364547c686bfaabca472eaac
An Jihyun <hwiyu25@gmail.com>
2021-07-06 13:52:18 +0900

    💄 flexbox mixin 생성

---
+++ src/mixins/flexbox.js
@@ -0,0 +1,15 @@
+import { css } from "styled-components";
+
+const Flexbox = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FlexBoxColumn = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export { Flexbox, FlexBoxColumn };
