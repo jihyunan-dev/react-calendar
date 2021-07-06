c31487b2312c145456b08fa32918885517f0595d
An Jihyun <hwiyu25@gmail.com>
2021-07-06 18:42:02 +0900

    🚚 Calendar, Add 페이지 라우트 연결

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/configStore";
 import App from "./shared/App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
+import { Link, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import Calendar from "../pages/Calendar";
 import theme from "./theme";
+import Add from "../pages/Add";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div>
         <GlobalStyles />
-        <Calendar />
+        <Route path="/" exact component={Calendar} />
+        <Route path="/add" component={Add} />
       </div>
     </ThemeProvider>
   );
