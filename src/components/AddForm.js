1cbc0e2cd85924b14f581f227ef7bb1b82efaf1d
An Jihyun <hwiyu25@gmail.com>
2021-07-07 23:36:09 +0900

    🎨 Add page에서 form을 별도 컴포넌트(AddForm)으로 분리

---
+++ src/components/AddForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import moment from "moment";
+import { useDispatch } from "react-redux";
+
+import { actionCreators as calendarActions } from "../redux/modules/calendar";
+import { Input, RectangleBtn, CancleBtn } from "../elements";
+import { flex } from "../mixins";
+
+const AddForm = (props) => {
+  const { history } = props;
+
+  const dispatch = useDispatch();
+
+  // ⭐반드시 리팩토링 하기
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+  const [memo, setMemo] = useState("");
+  const [location, setLocation] = useState("");
+  // color는 나중에 버튼으로 추가 예정
+  const [color, setColor] = useState("red");
+
+  const handleSubmit = (e) => {
+    const schedule = {
+      title,
+      date: parseInt(date.split("-").join("")),
+      location,
+      memo,
+      color,
+      query: parseInt(date.slice(0, 5)),
+      isCompleted: false,
+    };
+    dispatch(calendarActions.addCalendarFB(schedule));
+    history.push("/");
+  };
+  return (
+    <Form>
+      <Input
+        id="title"
+        label="일정 제목*"
+        type="text"
+        _onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+
+      <Input
+        id="location"
+        label="장소"
+        type="text"
+        _onChange={(e) => setLocation(e.target.value)}
+        value={location}
+      />
+      <Input
+        id="date"
+        label="날짜*"
+        type="date"
+        _onChange={(e) => setDate(e.target.value)}
+        value={date}
+      />
+      <Input
+        id="memo"
+        label="메모"
+        type="textarea"
+        _onChange={(e) => setMemo(e.target.value)}
+        value={memo}
+      />
+      <BtnBox>
+        <CancleBtn to="/">취소</CancleBtn>
+        <RectangleBtn type="button" onClick={handleSubmit}>
+          일정 추가하기
+        </RectangleBtn>
+      </BtnBox>
+    </Form>
+  );
+};
+
+const Form = styled.form`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const BtnBox = styled.div`
+  ${flex.Flexbox};
+  justify-content: flex-end;
+  gap: 20px;
+`;
+
+export default AddForm;
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,81 +1,22 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Input } from "../elements";
-import moment from "moment";
-import { actionCreators as calendarActions } from "../redux/modules/calendar";
+import React from "react";
+import styled from "styled-components";
+
+import { flex } from "../mixins";
+import AddForm from "../components/AddForm";
 
 const Add = (props) => {
-  const { history } = props;
-
-  const dispatch = useDispatch();
-
-  const [isImportant, setIsImportant] = useState(false);
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
-  const [memo, setMemo] = useState("");
-  const [location, setLocation] = useState("");
-  // color는 나중에 버튼으로 추가 예정
-  const [color, setColor] = useState("red");
-
-  const handleSubmit = (e) => {
-    const schedule = {
-      isImportant,
-      title,
-      date: parseInt(date.split("-").join("")),
-      location,
-      memo,
-      color,
-      query: parseInt(date.slice(0, 5)),
-      isCompleted: false,
-    };
-    dispatch(calendarActions.addCalendarFB(schedule));
-    history.push("/");
-  };
-
   return (
-    <>
-      <form>
-        <Input
-          id="is-important"
-          label="중요"
-          type="checkbox"
-          _onChange={(e) => setIsImportant(e.target.checked)}
-        />
-        <Input
-          id="title"
-          label="일정 제목"
-          type="text"
-          _onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
-
-        <Input
-          id="location"
-          label="장소"
-          type="text"
-          _onChange={(e) => setLocation(e.target.value)}
-          value={location}
-        />
-        <Input
-          id="date"
-          label="날짜"
-          type="date"
-          _onChange={(e) => setDate(e.target.value)}
-          value={date}
-        />
-        <Input
-          id="memo"
-          label="메모"
-          type="textarea"
-          _onChange={(e) => setMemo(e.target.value)}
-          value={memo}
-        />
-        <button type="button" onClick={handleSubmit}>
-          일정 추가하기
-        </button>
-      </form>
-    </>
+    <Container>
+      <AddForm />
+    </Container>
   );
 };
+
+const Container = styled.section`
+  ${flex.FlexBoxColumn};
+  max-width: 500px;
+  margin: 50px auto;
+  padding: 0 30px;
+`;
 
 export default Add;
